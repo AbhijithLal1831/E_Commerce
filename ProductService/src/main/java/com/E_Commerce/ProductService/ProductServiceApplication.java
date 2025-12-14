661f7f898662584ee4de454b5c820bda33bde782
Abhijith Lal <abhijithlalharivaram@gmail.com>
2025-12-14 20:56:39 +0530

    Initial commit

---
+++ ProductService/src/main/java/com/E_Commerce/ProductService/ProductServiceApplication.java
@@ -0,0 +1,14 @@
+package com.E_Commerce.ProductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
